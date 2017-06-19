bd02a96979e438115fe6f13a8f02e894ecc620d8
Luke Childs <lukechilds123@gmail.com>
2017-06-19 18:15:01 +0700

    Test Response() cannot be invoked without 'new'

--- test/responselike.js
+++ test/responselike.js
@@ -4,3 +4,8 @@ import Response from '../';
 test('Response is a function', t => {
 	t.is(typeof Response, 'function');
 });
+
+test('Response cannot be invoked without \'new\'', t => {
+	t.throws(() => Response()); // eslint-disable-line new-cap
+	t.notThrows(() => new Response());
+});

173c07017786506ff24706c1c2e1b8a9df58ba4c
Luke Childs <lukechilds123@gmail.com>
2017-06-21 13:06:53 +0700

    Check headers type

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ class Response extends Readable {
 		if (typeof statusCode !== 'number') {
 			throw new TypeError('Argument `statusCode` should be a number');
 		}
+		if (typeof headers !== 'object') {
+			throw new TypeError('Argument `headers` should be an object');
+		}
+
 		super();
 		this.statusCode = statusCode;
 		this.headers = headers;
